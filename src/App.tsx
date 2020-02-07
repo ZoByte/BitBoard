0ff5527cfd8909756c38b6a009cb3dc736d3e161
Danil Korennykh <bluskript@gmail.com>
2020-02-07 01:02:18 -0500

    background photos are nice

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Message } from './Components/Message';
 const App = () => {
   return (
     <div className="App">
+      <div style={{width: '100vw', height: '100vh', backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundImage: 'url(https://source.unsplash.com/1920x1080/?mountains)'}}></div>
       <Message message="hi" position={{x:"15%", y:"3%", width: "50%"}} />
     </div>
   );
